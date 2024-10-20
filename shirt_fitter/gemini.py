ccf5564355653ee0449049cec4e2f95148998241
ethankou <bluecheetos666@gmail.com>
2024-10-19 20:41:10 -0700

    try catch for gemini

--- shirt_fitter/gemini.py
+++ shirt_fitter/gemini.py
@@ -31,7 +31,6 @@ model = genai.GenerativeModel(
 # loop for video
 path = 'shirt_fitter/gemini_images'  # path to folder of images
 cam = cv2.VideoCapture(0)
-n = 0
 while True:
     ret, frame = cam.read()
     frame = cv2.resize(frame, (640, 360))
@@ -39,25 +38,27 @@ while True:
     
     if cv2.waitKey(1) == ord('0'):
         print('capturing image')
-        img_path = path + f'/test{n}.png'
+        img_path = path + f'/test0.png'
         cv2.imwrite(img_path, frame)  # add to folder
         print(img_path)
 
         prompt_parts = [
             upload_to_gemini(img_path, mime_type="image/png"),
-            "You are a fashion expert who gives fashion advice to people. Your job is to analyze a photo of me and give me 3 specific recommendations of shirts from various brands. Output the 3 clothes as a JSON dictionary that includes the brand, shirt type, and color. Also, provide a concise and simple description of what shirt I am wearing. Reference me in 2nd person."
+            "You are a fashion expert who gives fashion advice to people. Your job is to analyze a photo of me and give me 3 specific recommendations of t-shirts from various brands. Output the 3 clothes as a JSON dictionary that includes the brand, shirt type, and color. Also, provide a concise and simple description of what shirt I am wearing. Reference me in 2nd person."
         ]
- 
-        response = model.generate_content(prompt_parts)
-        deez = json.loads(response.text)
-        description = deez['description']
-        recs_dict = deez['recommendations']
-        recs = [f'{n['brand']} {n['color']} {n['shirt_type']}' for n in recs_dict]
+
+        try:
+            response = model.generate_content(prompt_parts)
+            deez = json.loads(response.text)
+            description = deez['description']
+            recs_dict = deez['recommendations']
+            recs = [f'{n['brand']} {n['color']} {n['shirt_type']}' for n in recs_dict]
+        except:
+            print('Please try again.')
 
         print(description)  # a string description of what shirt the person in the image is wearing
         print(recs)  # array of 3 strings. each string is a shirt recommendation
 
-        n += 1
 
     if cv2.waitKey(1) == ord('q'):
         break
