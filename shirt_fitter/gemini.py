0f15b94dbdffcc7afc56a29b94faa3c9c9ad7fe6
ethankou <bluecheetos666@gmail.com>
2024-10-19 21:04:08 -0700

    made gemini_run function

--- shirt_fitter/gemini.py
+++ shirt_fitter/gemini.py
@@ -15,6 +15,27 @@ def upload_to_gemini(path, mime_type=None):
     print(f"Uploaded file '{file.display_name}' as: {file.uri}")
     return file
 
+def gemini_run(frame):
+    img_path = 'shirt_fitter/gemini_images/test0.png'
+    cv2.imwrite(img_path, frame)  # add to folder
+
+    prompt_parts = [
+        upload_to_gemini(img_path, mime_type="image/png"),
+        "You are a fashion expert who gives fashion advice to people. Your job is to analyze a photo of me and give me 3 specific recommendations of t-shirts from various brands. Output the 3 clothes as a JSON dictionary that includes the brand, shirt type, and color. Also, provide a concise and simple description of what shirt I am wearing. Reference me in 2nd person."
+    ]
+
+    try:
+        response = model.generate_content(prompt_parts)
+        deez = json.loads(response.text)
+        description = deez['description']
+        recs_dict = deez['recommendations']
+        recs = [f'{n['brand']} {n['color']} {n['shirt_type']}' for n in recs_dict]
+        return description, recs
+    except:
+        print('please try again')
+        return None
+
+
 generation_config = {
   "temperature": 0,
   "top_p": 0.95,
@@ -37,28 +58,13 @@ while True:
     cv2.imshow('frame', frame)
     
     if cv2.waitKey(1) == ord('0'):
-        print('capturing image')
-        img_path = path + f'/test0.png'
-        cv2.imwrite(img_path, frame)  # add to folder
-        print(img_path)
-
-        prompt_parts = [
-            upload_to_gemini(img_path, mime_type="image/png"),
-            "You are a fashion expert who gives fashion advice to people. Your job is to analyze a photo of me and give me 3 specific recommendations of t-shirts from various brands. Output the 3 clothes as a JSON dictionary that includes the brand, shirt type, and color. Also, provide a concise and simple description of what shirt I am wearing. Reference me in 2nd person."
-        ]
-
-        try:
-            response = model.generate_content(prompt_parts)
-            deez = json.loads(response.text)
-            description = deez['description']
-            recs_dict = deez['recommendations']
-            recs = [f'{n['brand']} {n['color']} {n['shirt_type']}' for n in recs_dict]
-        except:
-            print('Please try again.')
-
-        print(description)  # a string description of what shirt the person in the image is wearing
-        print(recs)  # array of 3 strings. each string is a shirt recommendation
-
+        output = gemini_run(frame)  # if an error occured, output equals to None
+        if output:  # check if there was an output
+            description, recs = output
+            print(description)
+            print(recs)
+        else:
+            print('Try again')
 
     if cv2.waitKey(1) == ord('q'):
         break
